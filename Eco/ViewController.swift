2e3caeec629e997835b4d8fc8c6a20d1ed98174f
matteo <matteobarni92@gmail.com>
2018-01-20 22:57:19 +0100

    Created XcodeProject

---
+++ Eco/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Eco
+//
+//  Created by matteo barni on 20/01/18.
+//  Copyright © 2018 matteo barni. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
